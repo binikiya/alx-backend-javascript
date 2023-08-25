9b3353f0978fd6c194af88285e8f70b2314f1e2f
Biniyam <biniyamkiya3682@gmail.com>
2023-08-25 09:34:50 +0300

    Node Js Basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Biniyam Tamene <https://github.com/binikiya>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
